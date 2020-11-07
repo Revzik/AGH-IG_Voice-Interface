b815d0a25917bd61529eb4ae1875abea06da7b9d
revzik <b.piekarz1997@gmail.com>
2020-11-07 21:23:59 +0100

    Fix order in unit test

--- tests/conf/test_configurator.py
+++ tests/conf/test_configurator.py
@@ -7,37 +7,37 @@ class ConfiguratorTest(unittest.TestCase):
         config.reload()
 
     def test_init(self):
-        self.assertEqual(config.analysis_config.sections(), ['preprocessing', 'parametrization'])
-        self.assertEqual(config.synthesis_config.sections(), [])
+        self.assertEqual(['preprocessing', 'parametrization'], config.analysis_config.sections())
+        self.assertEqual([], config.synthesis_config.sections())
 
     def test_parameters_types(self):
         # analysis
-        self.assertEqual(type(config.analysis["sampling_frequency"]), int)
-        self.assertEqual(type(config.analysis["preemphasis"]), float)
-        self.assertEqual(type(config.analysis["speech_threshold"]), float)
-        self.assertEqual(type(config.analysis["window_length"]), int)
-        self.assertEqual(type(config.analysis["window_overlap"]), int)
-        self.assertEqual(type(config.analysis["filterbank_size"]), int)
+        self.assertEqual(int, type(config.analysis["sampling_frequency"]))
+        self.assertEqual(float, type(config.analysis["preemphasis"]))
+        self.assertEqual(float, type(config.analysis["speech_threshold"]))
+        self.assertEqual(int, type(config.analysis["window_length"]))
+        self.assertEqual(int, type(config.analysis["window_overlap"]))
+        self.assertEqual(int, type(config.analysis["filterbank_size"]))
 
     def test_default_parameters(self):
-        self.assertEqual(config.analysis["sampling_frequency"], 8000)
-        self.assertEqual(config.analysis["preemphasis"], 0.95)
-        self.assertEqual(config.analysis["speech_threshold"], 0.4)
-        self.assertEqual(config.analysis["window_length"], 20)
-        self.assertEqual(config.analysis["window_overlap"], 10)
-        self.assertEqual(config.analysis["filterbank_size"], 14)
+        self.assertEqual(8000, config.analysis["sampling_frequency"])
+        self.assertEqual(0.95, config.analysis["preemphasis"])
+        self.assertEqual(0.4, config.analysis["speech_threshold"])
+        self.assertEqual(20, config.analysis["window_length"])
+        self.assertEqual(10, config.analysis["window_overlap"])
+        self.assertEqual(14, config.analysis["filterbank_size"])
 
     def test_parameters_fallbacks(self):
         # analysis
         config.analysis_config.clear()
         config.analysis = config.parse_analysis()
 
-        self.assertEqual(config.analysis["sampling_frequency"], 8000)
-        self.assertEqual(config.analysis["preemphasis"], 0.95)
-        self.assertEqual(config.analysis["speech_threshold"], 0.4)
-        self.assertEqual(config.analysis["window_length"], 20)
-        self.assertEqual(config.analysis["window_overlap"], 10)
-        self.assertEqual(config.analysis["filterbank_size"], 14)
+        self.assertEqual(8000, config.analysis["sampling_frequency"])
+        self.assertEqual(0.95, config.analysis["preemphasis"])
+        self.assertEqual(0.4, config.analysis["speech_threshold"])
+        self.assertEqual(20, config.analysis["window_length"])
+        self.assertEqual(10, config.analysis["window_overlap"])
+        self.assertEqual(14, config.analysis["filterbank_size"])
 
 
 if __name__ == '__main__':
