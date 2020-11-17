20107b18caf3db4372ca712040910b00873151a1
AR_98 <aleksandrarogowiec@gmail.com>
2020-11-17 21:01:34 +0100

    Prepare downsample better

--- src/utils/sound_loader.py
+++ src/utils/sound_loader.py
@@ -6,16 +6,26 @@ import scipy.signal as sig
 class SoundLoader:
     def __init__(self):
         # tak się dobiera do ustalonych parametrów (wszystkie są w configurator.py)
-        print(config.analysis['sampling_frequency'])
+       pass
 
     def downsample(self, audio, fs):
+
         sampling_frequency = config.analysis['sampling_frequency']
-        nww = (fs * sampling_frequency) / math.gcd(fs, sampling_frequency)
-        up = int(nww // fs)
-        down = int(nww // sampling_frequency)
-        audio_up = np.zeros(len(audio) * up)  # zwiększamy sygnał
-        audio_up[1::up] = audio
-        filtr = sig.firwin(301, cutoff=sampling_frequency / 2, fs=fs * up)
-        audio_up = up * sig.filtfilt(filtr, 1, audio_up)
-        audio_down = audio_up[1::down]
+
+        #down and up factor
+        last_common_multipe = (fs * sampling_frequency) / math.gcd(fs, sampling_frequency)
+        upsample_factor = int(last_common_multipe// fs)
+        downsample_factor = int(last_common_multipe// sampling_frequency)
+
+        #upsampling
+        audio_up = np.zeros(len(audio) * upsample_factor)
+        audio_up[upsample_factor//2::upsample_factor] = audio
+
+        #filtering
+        filtr = sig.firwin(301, cutoff=sampling_frequency / 2, fs=fs * upsample_factor)
+        audio_up = downsample_factor * sig.filtfilt(filtr, 1, audio_up)
+
+        #downsampling
+        audio_down = audio_up[downsample_factor//2::downsample_factor]
+
         return audio_down
