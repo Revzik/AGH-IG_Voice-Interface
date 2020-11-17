ce9d08863b6219e13c21640274883cbd0559e8a9
AR_98 <aleksandrarogowiec@gmail.com>
2020-11-17 23:30:24 +0100

    Prepare normalization

--- src/utils/sound_utils.py
+++ src/utils/sound_utils.py
@@ -1,7 +1,27 @@
 from src.conf import config
-
+import math
 
 class SoundUtils:
     def __init__(self):
-        # tak się dobiera do ustalonych parametrów (wszystkie są w configurator.py)
-        print(config.analysis['sampling_frequency'])
+        pass
+
+def normalziation(self,audio):
+
+    square = 0
+    mean =0.0
+    root_mean_square = 0.0
+
+    #calculate square
+    for i in range(0, len(audio)):
+        square += (audio[i]**2)
+
+    #calculate mean
+    mean = (square / (float)(len(audio)))
+
+    #calculate root
+    root_mean_square = math.sqrt(mean)
+
+    # signal normalization
+    audio_normalized = audio / root_mean_square
+
+    return audio_normalized
