34adcb41b996cea8c6be0497d388a139f7e6efa5
kasia7625 <kasia7625>
2020-11-17 22:35:41 +0100

    Zmiany z review zaimplentowane ;)

--- tests/utils/test_sound_loader.py
+++ tests/utils/test_sound_loader.py
@@ -4,15 +4,7 @@ from src.utils.sound_loader import SoundLoader as Sl
 
 
 class SoundLoaderTest(unittest.TestCase):
-    # tutaj można pisać testy do metod z Mfcc (na wzór test_configurator.py)
-    def test_sound_load_file(self):
+    #tutaj można pisać testy do metod z Mfcc (na wzór test_configurator.py)
 
-        result = Sl.sound_load_file()
-        print(len(result))
-        print(result)
-        plt.plot(result[1]['wav'])
-        plt.show()
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
