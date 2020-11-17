dbc29c21cb294625d83c83c2b42ece8381cca78e
revzik <b.piekarz1997@gmail.com>
2020-11-17 23:40:19 +0100

    Add FFT for MFCC

--- src/param/mfcc.py
+++ src/param/mfcc.py
@@ -1,7 +1,45 @@
 from src.conf import config
+import numpy as np
 
 
 class Mfcc:
     def __init__(self):
-        # tak się dobiera do ustalonych parametrów (wszystkie są w configurator.py)
-        print(config.analysis['sampling_frequency'])
+        pass
+
+    def fft(self, x):
+        if x.size == 0:
+            return x
+        if x.size & (x.size - 1):
+            target_length = first_power_of_2(x.size)
+            x = np.concatenate((x, np.zeros(target_length - x.size)))
+
+        y = cooley_tukey(x)
+
+        return y
+
+
+def cooley_tukey(x):
+    if x.size == 1:
+        return x
+
+    y_even = cooley_tukey(x[0::2])
+    y_odd = cooley_tukey(x[1::2])
+
+    mod = -2j * np.pi * np.arange(0, x.size/2) / x.size
+    y_left = y_even + y_odd * np.exp(mod)
+    y_right = y_even - y_odd * np.exp(mod)
+
+    return np.concatenate((y_left, y_right))
+
+
+def first_power_of_2(x):
+    if x < 1:
+        return None
+    power = 1
+    while power < x:
+        power *= 2
+    return power
+
+
+if __name__ == '__main__':
+    mfcc = Mfcc()
