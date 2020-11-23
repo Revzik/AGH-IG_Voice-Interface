25cc6e04c74e2012dfde32a5fed5f07273c1443f
AleksandraR177 <73615361+AleksandraR177@users.noreply.github.com>
2020-11-23 14:00:10 +0100

    Prepare normalization (#41)
    * Prepare normalization
    
    * Prepare normalization - better
    
    * Prepare normalization - improvement
    
    * Prepare normalization - improvement better

--- src/utils/sound_utils.py
+++ src/utils/sound_utils.py
@@ -1,7 +1,17 @@
 from src.conf import config
-
+import numpy as np
+from src.param.mfcc import Mfcc
 
 class SoundUtils:
     def __init__(self):
-        # tak się dobiera do ustalonych parametrów (wszystkie są w configurator.py)
-        print(config.analysis['sampling_frequency'])
+        pass
+
+    def normalize(self, audio):
+
+        root_mean_square = np.sqrt(np.mean(np.power(audio, 2)))
+        # signal normalization
+        audio_normalize = audio / root_mean_square
+
+        return audio_normalize
+
+
--- tests/utils/test_sound_utils.py
+++ tests/utils/test_sound_utils.py
@@ -1,10 +1,16 @@
 import unittest
-
+from src.utils.sound_utils import SoundUtils
+import numpy as np
 
 class SoundUtilsTest(unittest.TestCase):
-    # tutaj można pisać testy do metod z Mfcc (na wzór test_configurator.py)
-    pass
+
+
+  def test_normalization(self):
+        samples = [1,2,3,4,5,6]
+        self.assertEqual(1, np.sqrt(np.mean(np.power(SoundUtils.normalize(samples), 2))))
+
 
 
 if __name__ == '__main__':
     unittest.main()
+
