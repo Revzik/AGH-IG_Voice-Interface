4b4cdcd4e2a1ecbee8be7829aafa5c98914a2d1f
AR_98 <aleksandrarogowiec@gmail.com>
2020-11-23 09:50:29 +0100

    Prepare normalization - improvement better

--- tests/utils/test_sound_utils.py
+++ tests/utils/test_sound_utils.py
@@ -3,13 +3,14 @@ from src.utils.sound_utils import SoundUtils
 import numpy as np
 
 class SoundUtilsTest(unittest.TestCase):
-    # tutaj można pisać testy do metod z Mfcc (na wzór test_configurator.py)
-    pass
+
+
+  def test_normalization(self):
+        samples = [1,2,3,4,5,6]
+        self.assertEqual(1, np.sqrt(np.mean(np.power(SoundUtils.normalize(samples), 2))))
+
 
 
 if __name__ == '__main__':
     unittest.main()
 
-    def test_normalization(self):
-        samples = [1,2,3,4,5,6]
-        self.assertEqual(1, np.sqrt(np.mean(np.power(SoundUtils.normalize(samples), 2))))
