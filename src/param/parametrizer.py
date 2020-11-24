4864f7a224efd4f2b97ccd5de43eff52cdd662a1
revzik <b.piekarz1997@gmail.com>
2020-11-25 00:43:49 +0100

    Refactor parametrizer.py

--- src/param/parametrizer.py
+++ src/param/parametrizer.py
@@ -1,7 +1,1 @@
 from src.conf import config
-
-
-class Parametrizer:
-    def __init__(self):
-        # tak się dobiera do ustalonych parametrów (wszystkie są w configurator.py)
-        print(config.analysis['sampling_frequency'])
