c059e657f79fc887bd46ff9af8f2b137a0caab07
revzik <b.piekarz1997@gmail.com>
2020-11-25 00:43:09 +0100

    Refactor mfcc.py

--- src/classes/containers.py
+++ src/classes/containers.py
@@ -6,10 +6,16 @@ from src.conf import config
 
 
 class SoundWave:
-    def __init__(self, samples=np.zeros(1), fs=44100, phrase=''):
+    def __init__(self, samples, fs=44100, phrase=''):
         self.samples = samples
         self.fs = fs
         self.phrase = phrase
+
+    def __getitem__(self, i):
+        return self.samples[i]
+
+    def __setitem__(self, i, value):
+        self.samples[i] = value
 
     def get_length(self):
         return self.samples.size
@@ -39,3 +45,71 @@ class SoundWave:
 
     def normalize(self):
         self.samples = self.samples / np.sqrt(np.mean(np.power(self.samples, 2)))
+
+
+class Window:
+    def __init__(self, samples, fs=None):
+        self.samples = samples
+        if fs is None:
+            self.fs = config.analysis['sampling_frequency']
+        else:
+            self.fs = fs
+
+    def __getitem__(self, i):
+        return self.samples[i]
+
+    def __setitem__(self, i, value):
+        self.samples[i] = value
+
+    def get_length(self):
+        return self.samples.size
+
+
+class FFTFrame:
+    def __init__(self, samples, df=1):
+        self.samples = samples
+        self.df = df
+
+    def __getitem__(self, i):
+        return self.samples[i]
+
+    def __setitem__(self, i, value):
+        self.samples[i] = value
+
+    def get_length(self):
+        return self.samples.size
+
+    def spectrum(self):
+        return np.abs(self.samples[0:self.get_length() // 2])
+
+
+class MelFrame:
+    def __init__(self, samples, n_filters=None):
+        self.samples = samples
+        if n_filters is None:
+            self.n_filters = config.analysis['filterbank_size']
+        else:
+            self.n_filters = n_filters
+
+    def __getitem__(self, i):
+        return self.samples[i]
+
+    def __setitem__(self, i, value):
+        self.samples[i] = value
+
+    def get_length(self):
+        return self.samples.size
+
+
+class CepstralFrame:
+    def __init__(self, samples):
+        self.samples = samples
+
+    def __getitem__(self, i):
+        return self.samples[i]
+
+    def __setitem__(self, i, value):
+        self.samples[i] = value
+
+    def get_length(self):
+        return self.samples.size
