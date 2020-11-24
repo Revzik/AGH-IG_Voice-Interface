d707a34eba863c79c6cb29a9be34f180baa0ba5e
revzik <b.piekarz1997@gmail.com>
2020-11-24 23:33:33 +0100

    Refactor sound_loader.py

---
+++ src/classes/containers.py
@@ -0,0 +1,38 @@
+import numpy as np
+import scipy.signal as sig
+import math
+
+from src.conf import config
+
+
+class SoundWave:
+    def __init__(self, samples=np.zeros(1), fs=44100, phrase=''):
+        self.samples = samples
+        self.fs = fs
+        self.phrase = phrase
+
+    def get_length(self):
+        return self.samples.size
+
+    def downsample(self):
+        sampling_frequency = config.analysis['sampling_frequency']
+
+        # down and up factor
+        last_common_multiple = (self.fs * sampling_frequency) / math.gcd(self.fs, sampling_frequency)
+        upsample_factor = int(last_common_multiple // self.fs)
+        downsample_factor = int(last_common_multiple // sampling_frequency)
+
+        # upsampling
+        audio_up = np.zeros(self.get_length() * upsample_factor)
+        audio_up[upsample_factor // 2::upsample_factor] = self.samples
+
+        # filtering
+        alias_filter = sig.firwin(301, cutoff=sampling_frequency / 2, fs=self.fs * upsample_factor)
+        audio_up = downsample_factor * sig.filtfilt(alias_filter, 1, audio_up)
+
+        # downsampling
+        audio_down = audio_up[downsample_factor // 2::downsample_factor]
+        self.samples = audio_down
+
+    def remove_dc(self):
+        self.samples = self.samples - np.mean(self.samples)
--- src/utils/sound_loader.py
+++ src/utils/sound_loader.py
@@ -1,56 +1,31 @@
-from src.conf import config
 import os
 import glob
 import soundfile as sf
-import math
-import numpy as np
-import scipy.signal as sig
-
-class SoundLoader:
-    def __init__(self):
-        # tak się dobiera do ustalonych parametrów (wszystkie są w configurator.py)
-        # print(config.analysis['sampling_frequency']) - zostawiam, żeby mieć wzór
-        pass
+from src.classes.containers import SoundWave
 
 
-    def sound_load_file():
-        i = 0
-        sound_list = []
-        print("Please enter the path for the folder with waves: ")
-        paths = input()
-        for filename in glob.glob(os.path.join(paths, '*.wav')):
-            wav_data, fs = sf.read(filename)
-            sound_list.append({"name": os.listdir(paths)[i][:-4], "fs": fs, "wav": wav_data})
-            i += 1
+def load_sound_file():
+    i = 0
+    sound_list = []
+    print("Please enter the path for the folder with waves: ")
+    paths = input()
+    for filename in glob.glob(os.path.join(paths, '*.wav')):
+        wav_data, fs = sf.read(filename)
+        sound_list.append(SoundWave(wav_data, fs, os.listdir(paths)[i][:-4]))
+        i += 1
 
-        return sound_list
+    return sound_list
 
-    def downsample(self, audio, fs):
 
-        sampling_frequency = config.analysis['sampling_frequency']
+def downsample(sound_list):
+    for i in range(len(sound_list)):
+        sound_list[i].downsample()
 
-        #down and up factor
-        last_common_multipe = (fs * sampling_frequency) / math.gcd(fs, sampling_frequency)
-        upsample_factor = int(last_common_multipe// fs)
-        downsample_factor = int(last_common_multipe// sampling_frequency)
+    return sound_list
 
-        #upsampling
-        audio_up = np.zeros(len(audio) * upsample_factor)
-        audio_up[upsample_factor//2::upsample_factor] = audio
 
-        #filtering
-        filtr = sig.firwin(301, cutoff=sampling_frequency / 2, fs=fs * upsample_factor)
-        audio_up = downsample_factor * sig.filtfilt(filtr, 1, audio_up)
+def remove_dc_offset(sound_list):
+    for i in range(len(sound_list)):
+        sound_list[i].remove_dc()
 
-        #downsampling
-        audio_down = audio_up[downsample_factor//2::downsample_factor]
-
-        return audio_down
-
-    def remove_dc_offset(sound_list):
-
-        audio_without_dc = []
-        for i in range(0, len(sound_list)):
-            audio_without_dc.append({"name": sound_list[i]["name"], "fs": sound_list[i]["fs"], "wav": (sound_list[i]['wav'] - np.mean(sound_list[i]['wav']))})
-
-        return audio_without_dc
+    return sound_list
