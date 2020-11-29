34a294eb66ccf136252267f5b46a2634491d49c3
Bartłomiej Piekarz <b.piekarz1997@gmail.com>
2020-11-29 14:05:00 +0100

    Mel filterbank (#47)
    * Add counting of mel frequencies
    
    * Add applying of mel filterbank
    
    * Add unit tests

--- src/param/mfcc.py
+++ src/param/mfcc.py
@@ -1,6 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-from src.classes.containers import FFTFrame
+from src.conf import config
+from src.classes.containers import FFTFrame, MelFrame
 
 
 def fft(window):
@@ -9,9 +11,9 @@ def fft(window):
 
     :param window: (Window) signal array to transform.
     If length is not a power of 2, then it gets padded with zeros.
-
     :return: (FFTFrame) FFT of x
     """
+
     x = window.samples
 
     if x.size == 0:
@@ -21,9 +23,8 @@ def fft(window):
         x = np.concatenate((x, np.zeros(target_length - x.size)))
 
     y = cooley_tukey(x)
-    df = get_df(y.size, window.fs)
 
-    return FFTFrame(y, df)
+    return FFTFrame(y, window.fs)
 
 
 def cooley_tukey(x):
@@ -31,9 +32,9 @@ def cooley_tukey(x):
     Computes FFT using recursive Cooley-Tukey algorithm given x length is a power of 2
 
     :param x: (1-D numpy array) signal array to transform
-
     :return: (1-D complex numpy array) FFT of x
     """
+
     if x.size == 1:
         return x
 
@@ -52,9 +53,9 @@ def first_power_of_2(x):
     Finds the first power of 2 above or equal to given number
 
     :param x: (1-D numpy array) given number (must be at least 1)
-
     :return: (int) the closest power of 2 above x
     """
+
     if x < 1:
         return None
     power = 1
@@ -63,5 +64,77 @@ def first_power_of_2(x):
     return power
 
 
-def get_df(frame_length, fs):
-    return fs / frame_length
+def apply_mel_filterbank(fft_frame, bottom_frequency, top_frequency, n_filters):
+    """
+    Generates and applies mel filterbank onto a given frequency spectrum
+
+    :param fft_frame: (FFTFrame) frame to be converted into mel coefficients
+    :param bottom_frequency: (float) bottom frequency for the mel filter bank
+    :param top_frequency: (float) top frequency for the mel filter bank
+    :param n_filters: (int) number of mel filters
+    :return: (MelFrame) mel coefficients of a given frame
+    """
+
+    spectrum = fft_frame.spectrum()
+    filter_frequencies = get_filter_frequencies(bottom_frequency, top_frequency, n_filters)
+    filter_bins = get_filter_bins(filter_frequencies, fft_frame.length(), fft_frame.fs)
+    mel_frame = MelFrame(np.zeros(n_filters), n_filters=n_filters)
+
+    for m in range(1, n_filters + 1):
+        h = np.zeros(spectrum.size)
+        for k in range(filter_bins[m - 1], filter_bins[m]):
+            h[k] = (k - filter_bins[m - 1]) / (filter_bins[m] - filter_bins[m - 1])
+        for k in range(filter_bins[m], filter_bins[m + 1]):
+            h[k] = (filter_bins[m + 1] - k) / (filter_bins[m + 1] - filter_bins[m])
+
+        mel_frame[m - 1] = np.sum(np.dot(spectrum, h))
+
+    return mel_frame
+
+
+def get_filter_frequencies(f_min, f_max, n_filters):
+    """
+    Returns filterbank base frequencies (n_filters + 2)
+
+    :param f_min: minimum frequency for filterbank
+    :param f_max: maximum frequency for filterbank
+    :param n_filters: number of filters
+    :param n_bins: number of spectrum bins
+    :return: (1-D ndarray) filter frequencies in Hertz
+    """
+
+    mel_min = get_mel(f_min)
+    mel_max = get_mel(f_max)
+    filter_frequencies = np.linspace(mel_min, mel_max, n_filters + 2)
+    filter_frequencies = np.array([get_frequency(f) for f in filter_frequencies])
+
+    return filter_frequencies
+
+
+def get_filter_bins(filter_freq, n_bins, fs):
+    """
+    Returns numbers of frequency bins for a specific fft_frame
+
+    :param filter_freq:
+    :param n_bins:
+    :param fs:
+    :return:
+    """
+
+    return np.array([int(np.round(f * n_bins / fs)) for f in filter_freq])
+
+
+def get_mel(frequency):
+    """
+    Convert frequency to corresponding mel
+    """
+
+    return 2595 * np.log10(1 + frequency / 700)
+
+
+def get_frequency(mel):
+    """
+    Convert mel to corresponding frequency
+    """
+
+    return 700 * (np.power(10, mel / 2595) - 1)
--- tests/param/test_mfcc.py
+++ tests/param/test_mfcc.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 import matplotlib.pyplot as plt
 
-from src.param.mfcc import first_power_of_2, fft
+from src.param.mfcc import *
 from src.classes.containers import Window
 
 
@@ -27,7 +27,7 @@ class MfccTest(unittest.TestCase):
         x1 = Window(np.sin(2 * 20 * np.pi * t1), fs1)
         y1 = fft(x1)
 
-        self.assertEqual(1024, y1.get_length())
+        self.assertEqual(1024, y1.length())
         self.assertLess(500, y1.spectrum()[20])
         self.assertAlmostEqual(-np.pi/2, np.angle(y1[20]), 2)
         self.assertFalse(any(np.abs(f) > 1 for f in np.concatenate((y1[0:19], y1[21:1003], y1[1005:1024]))))
@@ -41,7 +41,7 @@ class MfccTest(unittest.TestCase):
         x2 = Window(np.sin(2 * 100 * np.pi * t2), fs2)
         y2 = fft(x2)
 
-        self.assertEqual(2048, y2.get_length())
+        self.assertEqual(2048, y2.length())
         self.assertGreater(500, y2[205])
         self.assertFalse(any(np.abs(f) > 50 for f in np.concatenate((y2[0:198], y2[212:1836], y2[1850:2048]))))
 
@@ -52,7 +52,7 @@ class MfccTest(unittest.TestCase):
         x3[50] = 1
         y3 = fft(x3)
 
-        self.assertEqual(128, y3.get_length())
+        self.assertEqual(128, y3.length())
         self.assertFalse(any(np.abs(f) > 1.01 or np.abs(f) < 0.99 for f in y3))
 
         # transform 4: sawtooth wave, number of samples is a power of 2
@@ -63,7 +63,7 @@ class MfccTest(unittest.TestCase):
         x4 = Window(2 * (t4/0.5 - np.floor(0.5 + t4/0.5)), fs4)
         y4 = fft(x4)
 
-        self.assertEqual(256, y4.get_length())
+        self.assertEqual(256, y4.length())
         self.assertLess(80, y4.spectrum()[4])
         self.assertLess(y4.spectrum()[8], y4.spectrum()[4])
         self.assertLess(y4.spectrum()[12], y4.spectrum()[8])
@@ -100,11 +100,112 @@ class MfccTest(unittest.TestCase):
         #
         # s4 = y4.spectrum()
         # f4 = np.arange(0, fs4 / 2, y4.df)
-        # fig = plt.figure(2, figsize=(8, 6))
+        # fig = plt.figure(4, figsize=(8, 6))
         # ax = plt.subplot(2, 1, 1)
         # ax.plot(t4, x4.samples)
         # ax = plt.subplot(2, 1, 2)
         # ax.plot(f4, s4)
+        # fig.show()
+
+    def test_frequency_to_mel(self):
+        f1 = 1000
+        self.assertAlmostEqual(1000, get_mel(f1), 0)
+
+        f2 = 4000
+        self.assertAlmostEqual(2146, get_mel(f2), 0)
+
+    def test_mel_to_frequency(self):
+        m1 = 1000
+        self.assertAlmostEqual(1000, get_frequency(m1), 0)
+
+        m2 = 2000
+        self.assertAlmostEqual(3429, get_frequency(m2), 0)
+
+    def test_mel_frequencies(self):
+        lf1 = 300
+        hf1 = 4000
+        n1 = 14
+        b1 = 1000
+        fs1 = 8000
+
+        freqs = get_filter_frequencies(lf1, hf1, n1)
+        self.assertEqual(16, freqs.size)
+
+        bins = get_filter_bins(freqs, b1, fs1)
+        self.assertEqual(16, bins.size)
+
+        # # Plots just in case
+        # lf_p = 300
+        # tf_p = 4000
+        # fs_p = 8000
+        # n_p = 10
+        # b_p = 50
+        #
+        # filter_freq_p = get_filter_frequencies(lf_p, tf_p, n_p)
+        # filter_bins_p = get_filter_bins(filter_freq_p, b_p, fs_p)
+        #
+        # # Mel filters
+        # bottom_freq_p = filter_freq_p[:-2]
+        # top_freq_p = filter_freq_p[2:]
+        # filter_freq_p = filter_freq_p[1:-1]
+        # filter_bins_p = filter_bins_p[1:-1]
+        #
+        # fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+        # for i in range(filter_freq_p.size):
+        #     ax.plot([bottom_freq_p[i], filter_freq_p[i], top_freq_p[i]], [0, 1, 0])
+        # ax.set_title('Mel filters')
+        # fig.show()
+        #
+        # # Filter frequencies and corresponding frequency bins
+        # fig, axes = plt.subplots(2, 1, figsize=(8, 6))
+        # axes[0].stem(filter_freq_p, np.ones(filter_freq_p.size))
+        # axes[0].set_xlim([0, tf_p])
+        # axes[0].set_title('Central mel filter frequencies')
+        # axes[1].stem(filter_bins_p, np.ones(filter_bins_p.size))
+        # axes[1].set_xlim([0, b_p // 2])
+        # axes[1].set_title('Central mel filter frequencies in corresponding frequency bins')
+        # fig.show()
+
+    def test_mel_filterbank(self):
+        lf1 = 300
+        hf1 = 4000
+        np1 = 20
+        fs1 = 8000
+        l1 = 0.1
+        n1 = int(fs1 * l1)
+        t1 = np.linspace(0, l1, n1, False)
+        x1 = Window(np.sin(2 * 1000 * np.pi * t1), fs1)
+        y1 = fft(x1)
+
+        mf1 = apply_mel_filterbank(y1, lf1, hf1, np1)
+        self.assertTrue([mf1[6] >= m for m in mf1.samples])
+
+        # Plots just in case
+        # Applying filterbank
+        # lf_p = 300
+        # hf_p = 4000
+        # nf_p = 20
+        # fs_p = 8000
+        # l_p = 0.1
+        # n_p = int(fs_p * l_p)
+        # t_p = np.linspace(0, l_p, n_p, False)
+        # x_p = Window(np.sin(2 * 1000 * np.pi * t_p), fs_p)
+        # y_p = fft(x_p)
+        #
+        # mf_p = get_filter_frequencies(lf_p, hf_p, nf_p)
+        # m_p = apply_mel_filterbank(y_p, lf_p, hf_p, nf_p)
+        #
+        # fig, axes = plt.subplots(3, 1, figsize=(8, 9))
+        # f_p = np.arange(0, fs_p, y_p.df)
+        # axes[0].plot(f_p, y_p.spectrum())
+        # axes[0].set_xlim([0, f_p[f_p.size // 2]])
+        # axes[0].set_title('Spectrum')
+        # axes[1].plot(mf_p[1:-1], m_p.samples)
+        # axes[1].set_xlim([0, f_p[f_p.size // 2]])
+        # axes[1].set_title('Mel coefficients in frequency domain')
+        # axes[2].plot(np.arange(1, m_p.n_filters + 1), m_p.samples)
+        # axes[2].set_xlim([1, m_p.n_filters])
+        # axes[2].set_title('Mel coefficients')
         # fig.show()
 
 
