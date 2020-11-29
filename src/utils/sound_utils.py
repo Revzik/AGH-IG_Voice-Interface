d377e74800798d4b17e8809031252d1b5f4e027b
AR_98 <aleksandrarogowiec@gmail.com>
2020-11-29 13:21:00 +0100

    Prepare preemphasis

--- src/utils/sound_utils.py
+++ src/utils/sound_utils.py
@@ -1,7 +1,17 @@
 import numpy as np
+from src.conf import config
 
 
 def normalize(sound_wave):
+
     sound_wave.samples = sound_wave.samples / np.sqrt(np.mean(np.power(sound_wave.samples, 2)))
+
+    return sound_wave
+
+
+def preemphasis(sound_wave):
+
+    pre = config.analysis['preemphasis']
+    sound_wave.samples = np.append(sound_wave.samples[0], sound_wave.samples[1:] - pre * sound_wave.samples[:-1])
 
     return sound_wave
--- tests/utils/test_sound_utils.py
+++ tests/utils/test_sound_utils.py
@@ -1,5 +1,7 @@
 import unittest
 import numpy as np
+import scipy.signal as sig
+import matplotlib.pyplot as plt
 
 from src.classes.containers import SoundWave
 from src.utils import sound_utils
@@ -18,6 +20,25 @@ class SoundUtilsTest(unittest.TestCase):
             self.assertEqual(1, np.sqrt(np.mean(np.power(sound_wave.samples, 2))))
 
 
+    def test_preemphasis(self):
+
+        #fig, axs = plt.subplots(1, 3)
+        sound_wave = SoundWave(sig.unit_impulse(100, 'mid'))
+
+        #axs[0].plot(sound_wave.samples)
+        #axs[0].set_title('Delta function')
+
+        spectrum_sound_wave = np.abs(np.fft.fft(sound_wave.samples))
+        spectrum_sound_wave_after_pre = np.abs(np.fft.fft(sound_utils.preemphasis(sound_wave).samples))
+
+        #axs[1].plot(spectrum_sound_wave)
+        #axs[1].set_title('Delta function spectrum')
+        #axs[2].plot(spectrum_sound_wave_after_pre)
+        #axs[2].set_title('Delta function spectrum after preemphasis')
+        #plt.show()
+
+        self.assertLess(spectrum_sound_wave_after_pre[0], spectrum_sound_wave[0])
+        self.assertLess(spectrum_sound_wave_after_pre[0], spectrum_sound_wave_after_pre[int(sound_wave.length()/2)])
+
 if __name__ == '__main__':
     unittest.main()
-
